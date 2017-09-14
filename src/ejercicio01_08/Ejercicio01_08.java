fd8003d22ac5042701dafa79cc8c9795879dbc9d
user <user@192.168.1.2>
2017-09-14 02:49:09 -0500

    0107 commit

---
+++ src/ejercicio01_08/Ejercicio01_08.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio01_08;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio01_08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //(Area and perimeter of a circle) Write a program that displays the area and perimeter
+        //of a circle that has a radius of 5.5 using the following formula:
+	//perimeter = 2 * radius * pi
+        //area = radius * radius * pi
+        
+        System.out.println("Perimetro = ");
+	System.out.println(2 * 5.5 * 3.14159);
+	System.out.println("Area = ");
+        System.out.println(5.5 * 5.5 * 3.14159);
+    }
+    
+}
